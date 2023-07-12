ae59d7baa9b625dbb0eaea65fae7218b7149103b
Amigosouls <amigosouls022@gmail.com>
2023-07-12 08:42:11 +0530

    36th commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment={
     production:true,
-    userapi:"https://acecraft-json-2xo98oiet-amigosouls.vercel.app/Users",
-    cartapi:'https://acecraft-json-2xo98oiet-amigosouls.vercel.app/Cart',
-    productsapi:'https://acecraft-json-2xo98oiet-amigosouls.vercel.app/Products',
-    trialsapi:'https://acecraft-json-2xo98oiet-amigosouls.vercel.app/Trials',
-    eventsapi:'https://acecraft-json-2xo98oiet-amigosouls.vercel.app/Events',
-    schoolsapi:'https://acecraft-json-2xo98oiet-amigosouls.vercel.app/Schools',
-    cardsapi:'https://acecraft-json-2xo98oiet-amigosouls.vercel.app/Cards',
-    notesapi:'https://acecraft-json-2xo98oiet-amigosouls.vercel.app/Notes'
+    userapi:"https://acecraft-db-x0lx.onrender.com/Users",
+    cartapi:'https://acecraft-db-x0lx.onrender.com/Cart',
+    productsapi:'https://acecraft-db-x0lx.onrender.com/Products',
+    trialsapi:'https://acecraft-db-x0lx.onrender.com/Trials',
+    eventsapi:'https://acecraft-db-x0lx.onrender.com/Events',
+    schoolsapi:'https://acecraft-db-x0lx.onrender.com/Schools',
+    cardsapi:'https://acecraft-db-x0lx.onrender.com/Cards',
+    notesapi:'https://acecraft-db-x0lx.onrender.com/Notes'
 
 };
